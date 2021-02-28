725cc0640b8fb11187767d02c092166c6fb1ae4c
carterkioski <carterkioski@gmail.com>
2021-02-28 00:09:24 -0500

    finishing queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,6 +1,6 @@
 --1
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary  FROM employees as e
-LEFT JOIN salaries as s
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary  FROM employees AS e
+LEFT JOIN salaries AS s
 ON e.emp_no = s.emp_no;
 --2
 SELECT first_name, last_name, hire_date FROM employees
@@ -21,7 +21,20 @@ ON de.dept_no = d.dept_no
 SELECT first_name, last_name, sex FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
 --6
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name ='Sales'
 --7
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name ='Sales' OR d.dept_name = 'Development'
 --8
+SELECT last_name, COUNT(last_name) FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC
